36ef5137a87b33eaa5ffa2b0b140fc15fc88e67c
evelina-meijere <124715450+evelina-meijere@users.noreply.github.com>
2023-03-09 14:00:15 +0000

    sakums

--- build_heap.py
+++ build_heap.py
@@ -2,15 +2,60 @@
 
 
 def build_heap(data):
-    swaps = []
+    n = len(data)
+    levels = int(math.log2(n)) + 1
+    
+    swaps = 0
+    for level in range(levels-2, -1, -1):
+        for i in range(2**level - 1, 2**(level+1) - 1):
+            parent = i
+            left_child = 2*i + 1
+            right_child = 2*i + 2
+            if right_child < n and data[right_child] < data[left_child]:
+                smallest = right_child
+            else:
+                smallest = left_child
+            if smallest < n and data[smallest] < data[parent]:
+                data[parent], data[smallest] = data[smallest], data[parent]
+                swaps += 1
+                
+    return swaps
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
 
-    return swaps
-
 
 def main():
+    atbilde = input("F vai I?")
+    if "I" in atbilde:
+       n = int(input())
+       parents = list(map(int,input().split()))
+       assert len(data) == n
+       swaps = build_heap(data)
+       print(len(swaps))
+       for i, j in swaps:
+           print(i,j)
+    elif "F" in atbilde:
+         failanos = input()
+         file = './test/' + failanos
+         if "a" not in failanos:
+             try: 
+                 with open(file) as file1:
+                    n = int(file1.readline())
+                    parents = list(map(int, file1.readline().split()))
+                    assert len(data) == n
+                    swaps = build_heap(data)
+                    print(len(swaps))
+                    for i, j in swaps:
+                        print(i, j)
+             except Exception as kluda:
+                 print("kluda:", str(kluda))
+                 return
+         else:
+             print("nepareizs nosaukums")
+             return
+
+    print(compute_height(n, parents))
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
@@ -18,24 +63,20 @@ def main():
 
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+    
 
     # calls function to assess the data 
     # and give back all swaps
-    swaps = build_heap(data)
+    
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
 
 
     # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+
 
 
 if __name__ == "__main__":
